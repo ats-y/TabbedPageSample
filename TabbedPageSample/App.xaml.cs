32ec8a54af8c8c4b2ee910d59edfc0e153dea5ef
ats-y <london.rightside@gmail.com>
2020-02-08 10:55:58 +0900

    Prism適用。

--- TabbedPageSample/App.xaml.cs
+++ TabbedPageSample/App.xaml.cs
@@ -1,28 +1,39 @@
 ﻿using System;
+using Prism;
+using Prism.Ioc;
+using TabbedPageSample.ViewModels;
+using TabbedPageSample.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TabbedPageSample
 {
-    public partial class App : Application
+    public partial class App
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
         }
 
-        protected override void OnStart()
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
         {
+
         }
 
-        protected override void OnSleep()
+        protected override void OnInitialized()
         {
+            InitializeComponent();
+
+            // 起動直後にMainPageを表示する。
+            NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
-        protected override void OnResume()
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+
+            // View「MainPage」ViewModels「MainPageViewModel」を登録する。
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
         }
     }
 }

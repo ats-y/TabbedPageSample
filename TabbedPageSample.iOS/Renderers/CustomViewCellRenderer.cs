a89aa3948ae44668785dc52e4fa62ae939108031
ats-y <london.rightside@gmail.com>
2020-02-12 10:51:03 +0900

    カスタムレンダラーでListViewの選択色を変更。

---
+++ TabbedPageSample.iOS/Renderers/CustomViewCellRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TabbedPageSample.Controls;
+using TabbedPageSample.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomViewCell), typeof(CustomViewCellRenderer))]
+
+namespace TabbedPageSample.iOS.Renderers
+{
+    public class CustomViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            cell.SelectedBackgroundView = new UIView
+            {
+                BackgroundColor = ((CustomViewCell)item).SelectedBackgroundColor.ToUIColor()
+            };
+            return cell;
+        }
+    }
+}
